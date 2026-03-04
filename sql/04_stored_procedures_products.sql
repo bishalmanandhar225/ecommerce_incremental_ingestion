cfba0d2ba40d90f213314d2b63672e67acb691c9
Bishal Manandhar <besal.manandhar@gmail.com>
2026-03-03 19:30:22 -0600

    Added store procedure for products and fact tables

---
+++ sql/04_stored_procedures_products.sql
@@ -0,0 +1,17 @@
+CREATE OR ALTER PROCEDURE dbo.sp_load_dim_product
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    TRUNCATE TABLE dbo.dim_product;
+
+    INSERT INTO dbo.dim_product (StockCode, Description, UnitPrice, updated_at)
+    SELECT
+        s.StockCode,
+        MAX(s.Description) AS Description,
+        MAX(s.UnitPrice)   AS UnitPrice,
+        SYSUTCDATETIME()   AS updated_at
+    FROM dbo.stg_online_retail s
+    WHERE s.StockCode IS NOT NULL
+    GROUP BY s.StockCode;
+END;
